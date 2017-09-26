1f8c8bf9ec651db52cdaa43df31b0270379884ce
gabriel-fc <gfc@ic.ufal.br>
2017-09-25 21:30:15 -0300

    pasta huffman

---
+++ huffman/cmake-build-debug/CMakeFiles/huffman.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/huffman.dir/main.c.obj"
+  "huffman.pdb"
+  "huffman.exe"
+  "huffman.exe.manifest"
+  "libhuffman.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/huffman.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
