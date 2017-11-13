66e6cf2b033a49598b9ff4efa61244bef8af784f
gabriel-fc <gfc@ic.ufal.br>
2017-11-12 23:01:15 -0300

    plot libraries

---
+++ plot/src/LinkedList.c
@@ -0,0 +1,45 @@
+#include "../inc/LinkedList.h"
+
+
+
+struct _node{
+	int item;
+	_node *next;
+};
+
+_node* create_linked_list()
+{
+	return NULL;
+}
+
+int is_empty(_node *head)
+{
+	return (head == NULL);
+}
+
+_node* add(_node *head, int item)
+{
+	_node *new_node = (_node*) malloc(sizeof(_node));
+	new_node->item = item;
+	new_node->next = head;
+	return new_node;
+}
+
+_node * list_search(_node *head, int item,FILE *list_file)
+{
+	int i=1;
+	while (head != NULL) 
+	{
+			fprintf(list_file, "%d",i);
+			if(i != 1000000)
+				fprintf(list_file, "\n");
+		if (head->item == item){
+			return head;
+		}
+		++i;
+		head = head->next;
+	}
+
+}
+
+
